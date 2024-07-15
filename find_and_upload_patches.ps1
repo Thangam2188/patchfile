f0c04dc7e083d90801ab6a51d4a387f02be12949
Thangam2188 <thangam.v@sycamoreinformatics.com>
2024-07-15 17:00:17 +0530

    Update find_and_upload_patches.ps1

--- find_and_upload_patches.ps1
+++ find_and_upload_patches.ps1
@@ -12,8 +12,10 @@ function Scan-Patches {
     )
     Write-Output "Scanning for critical and important security patches on instance $InstanceId in region $AWSRegion"
 
-    $parameters = '{\"Operation\":[\"Scan\"],\"SeverityLevels\":[\"Critical\",\"Important\"]}'
-    $scanCommand = "aws ssm send-command --instance-ids $InstanceId --document-name 'AWS-RunPatchBaseline' --parameters $parameters --region $AWSRegion"
+    $parameters = '{"Operation":["Scan"],"SeverityLevels":["Critical","Important"]}'
+    $scanCommand = @"
+aws ssm send-command --instance-ids $InstanceId --document-name 'AWS-RunPatchBaseline' --parameters '$parameters' --region $AWSRegion
+"@
     try {
         $result = Invoke-Expression $scanCommand
         return $result
