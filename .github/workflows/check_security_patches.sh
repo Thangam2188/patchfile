5563f5bb1c718024bac02c3385558b1ab5abb9a7
Thangam2188 <thangam.v@sycamoreinformatics.com>
2024-11-11 18:50:07 +0530

    Update check_security_patches.sh

--- .github/workflows/check_security_patches.sh
+++ .github/workflows/check_security_patches.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Define variables
+BUCKET_NAME="testpatchscript"
+REGION="us-east-1"
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+OUTPUT_FILE="/tmp/security_patches.txt"
+
 # Generate the MFA code using oathtool
 MFA_CODE=$(oathtool --base32 --totp "$MFA_SECRET")
 echo "Generated MFA Code: $MFA_CODE"
@@ -8,27 +14,6 @@ echo "Generated MFA Code: $MFA_CODE"
 echo "MFA_SERIAL_ARN: $MFA_SERIAL_ARN"
 echo "AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"
 echo "AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY"
-
-# Attempt to get temporary credentials with debug information using sudo
-TEMP_CREDS=$(sudo aws sts get-session-token --serial-number $MFA_SERIAL_ARN --token-code $MFA_CODE --duration-seconds 3600 --output json --debug 2>&1)
-STATUS=$?
-
-# Check if the command was successful
-if [ $STATUS -ne 0 ]; then
-  echo "Error obtaining temporary credentials: $TEMP_CREDS"
-  exit $STATUS
-fi
-
-# Parse and set temporary credentials
-echo "::set-output name=aws_access_key_id::$(echo $TEMP_CREDS | jq -r '.Credentials.AccessKeyId')"
-echo "::set-output name=aws_secret_access_key::$(echo $TEMP_CREDS | jq -r '.Credentials.SecretAccessKey')"
-echo "::set-output name=aws_session_token::$(echo $TEMP_CREDS | jq -r '.Credentials.SessionToken')"
-
-# Define variables
-BUCKET_NAME="testpatchscript"
-REGION="us-east-1"  # Adjust if needed
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-OUTPUT_FILE="/tmp/security_patches.txt"
 
 # Check for critical and important security patches and save the output
 if command -v yum &> /dev/null; then
